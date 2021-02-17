e304b436aaff5a875dfc161f309046bc5ededfb3
HipyCas <hipycas@gmail.com>
2021-02-17 12:52:38 +0100

    🕑 Add work in progress to FeatureLab
    Add a full text work in progress component to the FeatureLab section

---
+++ src/views/Other/FeatureLab.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+		<ion-header :translucent="true">
+			<ion-toolbar>
+				<ion-buttons slot="start">
+					<ion-menu-button></ion-menu-button>
+				</ion-buttons>
+				<ion-title>Feature Lab</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<FullPageText
+        title="Work in progress"
+        text="We are currently working on Feature Lab, come back soon"
+      ></FullPageText>
+	</ion-page>
+</template>
+
+<script>
+import FullPageText from '@/components/FullPageText.vue';
+
+export default {
+	name: 'OtherFeatureLab',
+	components: { FullPageText },
+};
+</script>
+
+<style></style>

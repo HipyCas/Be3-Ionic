c6e41e9968612ec94709d2bdee97f1458d8af995
HipyCas <hipycas@gmail.com>
2021-02-19 19:45:39 +0100

    Start work on Feature Lab + store

--- src/store/index.js
+++ src/store/index.js
@@ -1,96 +1,176 @@
+/* eslint-disable no-prototype-builtins */
+import axios from 'axios';
 import { createStore } from 'vuex';
 
 const store = createStore({
-    state () {
-        return {
-            devices: [
-                {
-                    'id': 1,
-                    'name': 'First',
-                    'location': '8º5"45\'N 5º56"7\'O',
-                    'status': 0,
-                },
-                {
-                    'id': 2,
-                    'name': 'Second',
-                    'location': '8º5"45\'N 5º56"7\'O',
-                    'status': 1,
-                },
-                {
-                    'id': 3,
-                    'name': 'Third',
-                    'location': '8º5"45\'N 5º56"7\'O',
-                    'status': 1,
-                },
-                {
-                    'id': 4,
-                    'name': 'Fourth',
-                    'location': '8º5"45\'N 5º56"7\'O',
-                    'status': 2,
-                },
-            ],
-            records: [
-                {
-                    id: 1,
-                    'inside_temp': 28.5,
-                    'inside_hum': 89.7,
-                    'outside_temp': 19.8,
-                    'outside_hum': 78.9,
-                },
-                {
-                    id: 2,
-                    'inside_temp': 23.5,
-                    'inside_hum': 92.7,
-                    'outside_temp': 9.8,
-                    'outside_hum': 87.9,
-                },
-                {
-                    id: 3,
-                    'inside_temp': 38.5,
-                    'inside_hum': 76.7,
-                    'outside_temp': 29.8,
-                    'outside_hum': 72.9,
-                },
-                {
-                    id: 4,
-                    'inside_temp': 29.9,
-                    'inside_hum': 88.7,
-                    'outside_temp': 18.4,
-                    'outside_hum': 76.4,
-                },
-            ]
-        };
-    },
-    getters: {
-        devices(state) {
-            return state.devices;
-        },
-        device(store) {
-            return (id) => {
-                return store.devices.find(device => device.id === id);
-            };
-        },
-        filterDevices(store) {
-            return (filter, condition) => store.devices.filter(device => device[filter] == condition);
-        },
-        onlineDevices(store) {
-            return store.devices.filter(device => device.status == 0);
-        },
-        warningDevices(store) {
-            return store.devices.filter(device => device.status == 1);
-        },
-        offlineDevices(store) {
-            return store.devices.filter(device => device['status'] == 2);
-        },
-        records(store) {
-            return store.records;
-        },
-        record(store) {
-            return (id) => {
-                return store.records.find(record => record.id == id)
-            }
-        }
-    }
+	state() {
+		return {
+			devices: [
+				{
+					id: 1,
+					name: 'First',
+					location: '8º5"45\'N 5º56"7\'O',
+					status: 0,
+				},
+				{
+					id: 2,
+					name: 'Second',
+					location: '8º5"45\'N 5º56"7\'O',
+					status: 1,
+				},
+				{
+					id: 3,
+					name: 'Third',
+					location: '8º5"45\'N 5º56"7\'O',
+					status: 1,
+				},
+				{
+					id: 4,
+					name: 'Fourth',
+					location: '8º5"45\'N 5º56"7\'O',
+					status: 2,
+				},
+			],
+			records: [
+				{
+					id: 1,
+					inside_temp: 28.5,
+					inside_hum: 89.7,
+					outside_temp: 19.8,
+					outside_hum: 78.9,
+				},
+				{
+					id: 2,
+					inside_temp: 23.5,
+					inside_hum: 92.7,
+					outside_temp: 9.8,
+					outside_hum: 87.9,
+				},
+				{
+					id: 3,
+					inside_temp: 38.5,
+					inside_hum: 76.7,
+					outside_temp: 29.8,
+					outside_hum: 72.9,
+				},
+				{
+					id: 4,
+					inside_temp: 29.9,
+					inside_hum: 88.7,
+					outside_temp: 18.4,
+					outside_hum: 76.4,
+				},
+				{
+					id: 5,
+					inside_temp: 29.9,
+					inside_hum: 88.7,
+					outside_temp: 18.4,
+					outside_hum: 76.4,
+				},
+			],
+			features: {
+				forceDarkTheme: false,
+				customAccentColor: false,
+				customAccentColorValue: 'blue',
+				matrixOrgClient: true,
+				customViews: false,
+				customViewsValue: 0,
+			},
+		};
+	},
+	mutations: {
+		//* DB related
+		updateDevices(state, data) {
+			state.devices = JSON.parse(data);
+		},
+		updateRecords(state, data) {
+			state.records = JSON.parse(data);
+		},
+		//* Features
+		updateFeature(state, options) {
+			state.features[options.name] = options.value;
+		},
+	},
+	actions: {
+		loadData() {
+			axios
+				.get('localhost:9000/devices/')
+				.then((response) => this.mutations.updateDevices(response.data))
+				.catch((err) =>
+					console.error(`Caught error when requesting all devices: ${err}`)
+				);
+			axios
+				.get('localhost:9000/records/')
+				.then((response) => this.updateRecords(response.data))
+				.catch((err) =>
+					console.error(`Caught error when requesting all devices: ${err}`)
+				);
+		},
+		/**
+		 *
+		 * @param {*} context
+		 * @param {Object} options - An object with the name of the feature and the desired value
+		 * @param {String} options.name - The name of the feature to update
+		 * @param {*} options.value - The value the feature will be set to
+		 */
+		setFeature(context, options) {
+			return new Promise((resolve, reject) => {
+				if (!options.hasOwnProperty('name')) reject('No name found');
+				if (!options.hasOwnProperty('value')) reject('No value found');
+				try {
+					context.commit('updateFeature', options);
+					resolve();
+				} catch (e) {
+					reject(e);
+				}
+			});
+		},
+	},
+	getters: {
+		//* Devices
+		// All devices
+		devices(state) {
+			return state.devices;
+		},
+		// Device by id
+		device(store) {
+			return (id) => {
+				return store.devices.find((device) => device.id === id);
+			};
+		},
+		// Filtered list of devices
+		filterDevices(store) {
+			return (filter, condition) =>
+				store.devices.filter((device) => device[filter] == condition);
+		},
+		// Only online devices
+		onlineDevices(store) {
+			return store.devices.filter((device) => device.status == 0);
+		},
+		// Only warning devices
+		warningDevices(store) {
+			return store.devices.filter((device) => device.status == 1);
+		},
+		// Only offline/error devices
+		offlineDevices(state) {
+			return state.devices.filter((device) => device['status'] == 2);
+		},
+		//* Records
+		// All records
+		records(state) {
+			return state.records;
+		},
+		// Specific record by id
+		record(state) {
+			return (id) => {
+				return state.records.find((record) => record.id == id);
+			};
+		},
+		//* Features
+		features(state) {
+			return state.features;
+		},
+	},
 });
 
 export default store;
--- src/views/Other/FeatureLab.vue
+++ src/views/Other/FeatureLab.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-page>
+	<ion-page>
 		<ion-header :translucent="true">
 			<ion-toolbar>
 				<ion-buttons slot="start">
@@ -8,20 +8,239 @@
 				<ion-title>Feature Lab</ion-title>
 			</ion-toolbar>
 		</ion-header>
-		<ion-content>
-			<FullPageText
-        title="Work in progress"
-        text="We are currently working on Feature Lab, come back soon"
-      ></FullPageText>
+		<ion-content class="ion-padding">
+			<ion-list>
+				<ion-list-header><h3>Theming</h3></ion-list-header>
+				<ion-item>
+					<p>Force dark theme</p>
+					<ion-toggle
+						slot="end"
+						v-model="localFeatures.forceDarkTheme"
+					></ion-toggle>
+				</ion-item>
+				<ion-item>
+					<p>Custom accent color</p>
+					<ion-button
+						@click="colorPicker()"
+						:disabled="!customAccentColor"
+						:color="customAccentColorButtonColor"
+						slot="end"
+						>{{ color.toUpperCase() }}</ion-button
+					>
+					<ion-toggle slot="end" v-model="customAccentColor"></ion-toggle>
+				</ion-item>
+			</ion-list>
+			<ion-list>
+				<ion-list-header><h3>Sections</h3></ion-list-header>
+				<ion-item>
+					<p>Matrix.org client</p>
+					<ion-toggle
+						slot="end"
+						v-model="localFeatures.matrixOrgClient"
+					></ion-toggle>
+				</ion-item>
+				<ion-item>
+					<p>Custom views</p>
+					<ion-button
+						@click="customViewsPicker()"
+						:disabled="!customViewsEnabled"
+						:color="customViewsEnabledButtonColor"
+						slot="end"
+						>{{ customViews }}</ion-button
+					>
+					<ion-toggle slot="end" v-model="customViewsEnabled"></ion-toggle>
+				</ion-item>
+			</ion-list>
+		</ion-content>
 	</ion-page>
 </template>
 
 <script>
-import FullPageText from '@/components/FullPageText.vue';
+//import FullPageText from '@/components/FullPageText.vue';
+import {
+	IonPage,
+	IonHeader,
+	IonToolbar,
+	IonButtons,
+	IonMenuButton,
+	IonTitle,
+	IonContent,
+	IonList,
+	IonListHeader,
+	IonItem,
+	//IonNote,
+	IonToggle,
+	IonButton,
+	pickerController,
+} from '@ionic/vue';
 
 export default {
 	name: 'OtherFeatureLab',
-	components: { FullPageText },
+	components: {
+		//FullPageText,
+		IonPage,
+		IonHeader,
+		IonToolbar,
+		IonButtons,
+		IonMenuButton,
+		IonTitle,
+		IonContent,
+		IonList,
+		IonListHeader,
+		IonItem,
+		//IonNote,
+		IonToggle,
+		IonButton,
+	},
+	data() {
+		return {
+			localFeatures: {},
+			color: 'blue',
+			customAccentColor: false,
+			customViews: 0,
+			customViewsEnabled: false,
+		};
+	},
+	computed: {
+		customAccentColorButtonColor() {
+			if (!this.customAccentColor) return 'medium';
+			else if (this.color == 'blue') return 'primary';
+			else if (this.color == 'yellow') return 'warning';
+			else if (this.color == 'green') return 'success';
+			return 'light';
+		},
+		customViewsEnabledButtonColor() {
+			console.info(`Number of custom views: ${this.customViews}`);
+			if (!this.customAccentColor) return 'medium';
+			else if (this.customViews === 0) return 'light';
+			return 'primary';
+		},
+		features() {
+			return this.$store.getters.features;
+		},
+	},
+	methods: {
+		async colorPicker() {
+			let selectedIndex = -1;
+			switch (this.color) {
+				case 'blue':
+					selectedIndex = 0;
+					break;
+				case 'yellow':
+					selectedIndex = 1;
+					break;
+				case 'green':
+					selectedIndex = 2;
+					break;
+			}
+			const picker = await pickerController.create({
+				columns: [
+					{
+						name: 'color',
+						selectedIndex,
+						options: [
+							{
+								text: 'Blue',
+								value: 'blue',
+							},
+							{
+								text: 'Yellow',
+								value: 'yellow',
+							},
+							{
+								text: 'Green',
+								value: 'green',
+							},
+						],
+					},
+				],
+				buttons: [
+					{
+						text: 'Cancel',
+						role: 'cancel',
+					},
+					{
+						text: 'Default',
+						handler: () => {
+							this.color = 'blue';
+						},
+					},
+					{
+						text: 'Confirm',
+						handler: (value) => {
+							this.color = value.color.value;
+						},
+					},
+				],
+			});
+			picker.onDidDismiss().then(() => {
+				console.log('Dismissed picker');
+			});
+			return picker.present();
+		},
+		async customViewsPicker() {
+			let selectedIndex = this.customViews;
+			const picker = await pickerController.create({
+				columns: [
+					{
+						name: 'number',
+						selectedIndex,
+						options: [
+							{
+								text: '0',
+								value: 0,
+							},
+							{
+								text: '1',
+								value: 1,
+							},
+							{
+								text: '2',
+								value: 2,
+							},
+							{
+								text: '3',
+								value: 3,
+							},
+							{
+								text: '4',
+								value: 4,
+							},
+							{
+								text: '5',
+								value: 5,
+							},
+						],
+					},
+				],
+				buttons: [
+					{
+						text: 'Cancel',
+						role: 'cancel',
+					},
+					{
+						text: 'Default',
+						handler: () => {
+							this.customViews = 0;
+						},
+					},
+					{
+						text: 'Confirm',
+						handler: (value) => {
+							this.customViews = value.number.value;
+						},
+					},
+				],
+			});
+			picker.onDidDismiss().then(() => {
+				console.log('Dismissed customViews picker');
+			});
+			return picker.present();
+		},
+	},
+	mounted() {
+		this.localFeatures = this.features;
+	},
 };
 </script>
 

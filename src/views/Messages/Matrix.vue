34c2b1eddceb79a4ab1d4259dce7d1e84180457b
HipyCas <hipycas@gmail.com>
2021-02-20 14:42:33 +0100

    Add mock Matrix.vue component

---
+++ src/views/Messages/Matrix.vue
@@ -0,0 +1,18 @@
+<template>
+	<ion-page>
+		<ion-content>
+			Welcome to the Matrix
+		</ion-content>
+	</ion-page>
+</template>
+
+<script>
+import { IonPage, IonContent } from '@ionic/vue';
+
+export default {
+	name: 'MessagesMatrix',
+	components: { IonPage, IonContent },
+};
+</script>
+
+<style></style>

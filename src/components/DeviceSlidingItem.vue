eb698263bf8ac2d13d51e4f4ffd4dc197572236e
Hipy Cas <hipycas@gmail.com>
2021-03-20 01:06:30 +0100

    Redesign device sharing logic
    The device sharing (on the _MORE_ action sheet), is completely
    redesigned to a now working method.
    Earlier on, it did not properly work on all devices, and it did not
    correctly share the generated JSON file.
    
    The new method is based on the device public link.
    Now, the device public link is what actually gets shared (what you could
    previously only do through the premissions modal), and the share
    Capacitor API is called on this link.
    When the Share API is not available on some OS's and browsers, a
    fallback is ran, which copies the link to the clipboard after getting
    user confirmation.
    
    The next step is placing a toast to confirm that the link was copied to
    the clipboard.

--- src/components/DeviceSlidingItem.vue
+++ src/components/DeviceSlidingItem.vue
@@ -1,105 +1,164 @@
 <template>
-	<ion-item-sliding :inset="true">
-		<!-- Left side options: Delete (expandable) -->
-		<ion-item-options side="start" @ionSwipe="deleteConfirmation(device)">
-			<ion-item-option
-				@click="deleteConfirmation(device)"
-				color="danger"
-				expandable
-			>
-				<ion-icon
-					slot="top"
-					:md="trashBinSharp"
-					:ios="trashBinOutline"
-				></ion-icon>
-				Delete
-			</ion-item-option>
-		</ion-item-options>
-		<!-- Item content -->
-		<ion-item :router-link="`/devices/device/${device.id}`">
-			<ion-label>
-				<h6>{{ device.name }}</h6>
-				<p>{{ device.location }}</p>
-			</ion-label>
-		</ion-item>
-		<!-- Right side options: edit + more -->
-		<ion-item-options side="end" @ionSwipe="moreActionSheet(device)">
-			<ion-item-option color="primary">
-				<ion-icon slot="top" :md="createSharp" :ios="createOutline"></ion-icon>
-				Edit
-			</ion-item-option>
-			<ion-item-option
-				@click="moreActionSheet(device)"
-				color="light"
-				expandable
-			>
-				<ion-icon
-					slot="top"
-					:md="ellipsisHorizontalSharp"
-					:ios="ellipsisVerticalOutline"
-					@click="moreActionSheet"
-				></ion-icon>
-				More
-			</ion-item-option>
-		</ion-item-options>
-	</ion-item-sliding>
+  <ion-item-sliding :inset="true">
+    <!-- Left side options: Delete (expandable) -->
+    <ion-item-options side="start" @ionSwipe="deleteConfirmation(device)">
+      <ion-item-option
+        @click="deleteConfirmation(device)"
+        color="danger"
+        expandable
+      >
+        <ion-icon
+          slot="top"
+          :md="trashBinSharp"
+          :ios="trashBinOutline"
+        ></ion-icon>
+        Delete
+      </ion-item-option>
+    </ion-item-options>
+    <!-- Item content -->
+    <ion-item :router-link="`/devices/device/${device.id}`">
+      <ion-label>
+        <h6>{{ device.name }}</h6>
+        <p>{{ device.location }}</p>
+      </ion-label>
+    </ion-item>
+    <!-- Right side options: edit + more -->
+    <ion-item-options side="end" @ionSwipe="moreActionSheet(device)">
+      <ion-item-option color="primary">
+        <ion-icon slot="top" :md="createSharp" :ios="createOutline"></ion-icon>
+        Edit
+      </ion-item-option>
+      <ion-item-option
+        @click="moreActionSheet(device)"
+        color="light"
+        expandable
+      >
+        <ion-icon
+          slot="top"
+          :md="ellipsisHorizontalSharp"
+          :ios="ellipsisVerticalOutline"
+          @click="moreActionSheet"
+        ></ion-icon>
+        More
+      </ion-item-option>
+    </ion-item-options>
+  </ion-item-sliding>
 </template>
 
 <script>
+import { Plugins } from '@capacitor/core';
 import {
-	Plugins,
-	FilesystemDirectory,
-	FilesystemEncoding,
-} from '@capacitor/core';
-import {
-	IonItemSliding,
-	IonItem,
-	IonLabel,
-	IonItemOptions,
-	IonItemOption,
-	IonIcon,
-	actionSheetController,
-	alertController,
-	modalController,
+  IonItemSliding,
+  IonItem,
+  IonLabel,
+  IonItemOptions,
+  IonItemOption,
+  IonIcon,
+  actionSheetController,
+  alertController,
+  modalController,
 } from '@ionic/vue';
 import {
-	trashBinSharp,
-	trashBinOutline,
-	createSharp,
-	createOutline,
-	ellipsisHorizontalSharp,
-	ellipsisVerticalOutline,
-	analyticsSharp,
-	shareSocialSharp,
-	peopleCircleSharp,
+  trashBinSharp,
+  trashBinOutline,
+  createSharp,
+  createOutline,
+  ellipsisHorizontalSharp,
+  ellipsisVerticalOutline,
+  analyticsSharp,
+  shareSocialSharp,
+  peopleCircleSharp,
 } from 'ionicons/icons';
 import PermissionsModal from '../components/PermissionsModal.vue';
 
 export default {
-	name: 'DeviceSlidingItem',
-	props: ['device'],
-	components: {
-		IonItemSliding,
-		IonItem,
-		IonLabel,
-		IonItemOptions,
-		IonItemOption,
-		IonIcon,
-	},
-	data() {
-		return {};
-	},
-	methods: {
-		async error(device, e) {
-			const alert = await alertController.create({
-				header: 'Error sharing',
-				subheader: `Device ${device.name}`,
-				message: `The following error was risen while sharing the device: ${e}`,
-				buttons: ['OK'],
-			});
-			return alert.present();
-		},
-		async shareDevice(device) {
+  name: 'DeviceSlidingItem',
+  props: ['device'],
+  components: {
+    IonItemSliding,
+    IonItem,
+    IonLabel,
+    IonItemOptions,
+    IonItemOption,
+    IonIcon,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    async error(operation, device, e) {
+      const alert = await alertController.create({
+        header: 'Error sharing',
+        subheader: `Device ${device.name}`,
+        message: `The following error was risen while ${operation} the device: ${e}`,
+        buttons: ['OK'],
+      });
+      return alert.present();
+    },
+    async shareDevice(device) {
+      const { Share, Clipboard } = Plugins;
+      const share = async device => {
+        return new Promise((resolve, reject) => {
+          Share.share({
+            title: `Device ${device.name}`,
+            text: `Access the Be3 device ${this.device.name} through this link!`,
+            url: `https://www.be3dashboard.com/devices/shared/${device.permissions.url.code}`,
+          })
+            .then(() => resolve())
+            .catch(err => reject(err));
+        });
+      };
+      const fallback = async device => {
+        const clipboardAlert = await alertController.create({
+          header: 'Direct share not available',
+          subHeader: `Device ${device.name}`,
+          message:
+            'Your browser or platform does not allow native sharing. Instead, the URL for the shared will be stored in your clipboard.',
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+            },
+            {
+              text: 'Copy', //? Change by proceed
+              handler: () => {
+                Clipboard.write({
+                  string: `Access the Be3 device ${this.device.name} through this link! https://www.be3dashboard.com/devices/shared/${device.permissions.url.code}`,
+                });
+              },
+            },
+          ],
+        });
+
+        return clipboardAlert.present();
+      };
+      if (device.permissions.url.enabled) {
+        share(device).catch(() => fallback(device));
+      } else {
+        const alert = await alertController.create({
+          header: 'Enable link sharing',
+          subHeader: `Device ${device.name}`,
+          message:
+            'Enable public link sharing for this device. The link will be shared if enabled.',
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+            },
+            {
+              text: 'Enable',
+              handler: () => {
+                device.permissions.url.enabled = true;
+                share(device).catch(() => fallback(device));
+              },
+            },
+          ],
+        });
+        return alert.present();
+      }
+
+      //! THE CODE BELOW DOES NOT WORK PROPERLY
+      /*
 			console.log('Collect plugins');
 			const { Filesystem, Share } = Plugins;
 			console.log('Starting read...');
@@ -191,85 +250,86 @@ export default {
 					}
 				})
 				.catch((e) => this.error(device, e));
-		},
-		async openPermissions(device) {
-			const modal = await modalController.create({
-				component: PermissionsModal,
-				swipeToClose: true, //? Revert, may be problematic for updating permissions in store and db
-				componentProps: {
-					title: `Device ${device.name} Permissions`,
-					type: 'device',
-					device,
-				},
-			});
-			return modal.present();
-		},
-		async moreActionSheet(device) {
-			const actionSheet = await actionSheetController.create({
-				header: `Device ${device.name}`,
-				buttons: [
-					{
-						text: 'View Data',
-						icon: analyticsSharp,
-						handler: () => {
-							console.log('Clicked view data');
-						},
-					},
-					{
-						text: 'Share',
-						icon: shareSocialSharp,
-						handler: () => {
-							console.log('Clicked share');
-							this.shareDevice(device);
-						},
-					},
-					{
-						text: 'Permissions',
-						icon: peopleCircleSharp,
-						handler: () => {
-							console.log('Clicked permissions');
-							this.openPermissions(device);
-						},
-					},
-				],
-			});
-			return actionSheet.present();
-		},
-		async deleteConfirmation(device) {
-			const alert = await alertController.create({
-				header: 'Confirm Delete',
-				subHeader: `Device ${device.name}`,
-				message: `Are you sure you want to delete this device?<br><strong>THIS CANNOT BE UNDONE</strong>`,
-				buttons: [
-					{
-						text: 'Cancel',
-						role: 'cancel',
-						handler: () => {
-							console.log(`Device #${device.id} NOT deleted`);
-						},
-					},
-					{
-						text: 'Delete',
-						cssClass: 'delete',
-						handler: () => {
-							console.log(`Device #${device.id} deleted`);
-						},
-					},
-				],
-			});
-			return alert.present();
-		},
-	},
-	setup() {
-		return {
-			trashBinSharp,
-			trashBinOutline,
-			createSharp,
-			createOutline,
-			ellipsisHorizontalSharp,
-			ellipsisVerticalOutline,
-		};
-	},
+				*/
+    },
+    async openPermissions(device) {
+      const modal = await modalController.create({
+        component: PermissionsModal,
+        swipeToClose: true, //? Revert, may be problematic for updating permissions in store and db
+        componentProps: {
+          title: `Device ${device.name} Permissions`,
+          type: 'device',
+          device,
+        },
+      });
+      return modal.present();
+    },
+    async moreActionSheet(device) {
+      const actionSheet = await actionSheetController.create({
+        header: `Device ${device.name}`,
+        buttons: [
+          {
+            text: 'View Data',
+            icon: analyticsSharp,
+            handler: () => {
+              console.log('Clicked view data');
+            },
+          },
+          {
+            text: 'Share',
+            icon: shareSocialSharp,
+            handler: () => {
+              console.log('Clicked share');
+              this.shareDevice(device);
+            },
+          },
+          {
+            text: 'Permissions',
+            icon: peopleCircleSharp,
+            handler: () => {
+              console.log('Clicked permissions');
+              this.openPermissions(device);
+            },
+          },
+        ],
+      });
+      return actionSheet.present();
+    },
+    async deleteConfirmation(device) {
+      const alert = await alertController.create({
+        header: 'Confirm Delete',
+        subHeader: `Device ${device.name}`,
+        message: `Are you sure you want to delete this device?<br><strong>THIS CANNOT BE UNDONE</strong>`,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log(`Device #${device.id} NOT deleted`);
+            },
+          },
+          {
+            text: 'Delete',
+            cssClass: 'delete',
+            handler: () => {
+              console.log(`Device #${device.id} deleted`);
+            },
+          },
+        ],
+      });
+      return alert.present();
+    },
+  },
+  setup() {
+    return {
+      trashBinSharp,
+      trashBinOutline,
+      createSharp,
+      createOutline,
+      ellipsisHorizontalSharp,
+      ellipsisVerticalOutline,
+    };
+  },
 };
 </script>
 
